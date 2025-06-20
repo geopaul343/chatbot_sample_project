4a3b736557ce92ca14da5b8010e50e49ac6665b5
Geopaul94 <geopaul94@gmail.com>
2025-06-20 20:13:58 +0530

    version updated no other changes commanted versioncode and version name in the buildgradle kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,8 +47,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.0"
+       
     }
 
     buildTypes {
